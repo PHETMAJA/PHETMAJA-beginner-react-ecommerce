40b50cc1b562b44a72edfdaf988106db8465a14b
PHETMAJA <Thanachot698@gmail.com>
2023-01-31 08:21:18 +0700

    Create-React-App

---
+++ phetmaja/frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">phetmaja</a>
+      </header>
+      <main>List products</main>
+    </div>
+  );
+}
+
+export default App;
