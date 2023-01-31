f6be4a5e1dafdc34487e3c1cf1a2af6d1ea90cc3
PHETMAJA <thanachot698@gmail.com>
2023-01-31 21:05:02 +0700

    Add-Bootstrap

--- phetmaja/frontend/src/App.js
+++ phetmaja/frontend/src/App.js
@@ -1,20 +1,35 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">phetmaja</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>phetmaja</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+          {/* <Link to="/">phetmaja</Link> */}
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} exact />
-            <Route path="/product/:slug" element={<ProductScreen />} exact />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} exact />
+              <Route path="/product/:slug" element={<ProductScreen />} exact />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
