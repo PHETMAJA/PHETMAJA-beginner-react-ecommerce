7172c2bf258849ed836602c8f1930049ef50c212
PHETMAJA <thanachot698@gmail.com>
2023-02-01 17:33:29 +0700

    Create-Product-Component

---
+++ phetmaja/frontend/src/components/Rating.js
@@ -0,0 +1,65 @@
+function Rating(props) {
+  const { rating, numReviews } = props;
+  return (
+    <div className="rating">
+      <span>
+        <i
+          className={
+            rating >= 1
+              ? 'fas fa-star'
+              : rating >= 0.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 2
+              ? 'fas fa-star'
+              : rating >= 0.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 3
+              ? 'fas fa-star'
+              : rating >= 0.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 4
+              ? 'fas fa-star'
+              : rating >= 0.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 5
+              ? 'fas fa-star'
+              : rating >= 0.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <span>{numReviews} reviwes</span>
+    </div>
+  );
+}
+
+export default Rating;
